efd2ff207c7cd3cbb5b975a0c0c9a04833ba1c15
Leonardo Custodio <leonardo@custodio.me>
2025-11-22 02:33:23 -0300

    Add tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,19 +3,11 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
     entry: ['src/index.ts'],
     outDir: 'dist',
-    format: ['cjs', 'esm', 'iife'],
+    format: ['iife', 'cjs', 'esm'],
     dts: true,
     sourcemap: true,
     clean: true,
     target: 'es2020',
-    platform: 'neutral',
-    external: [
-        'byte-data',
-        'collections/sorted-map',
-    ],
     globalName: 'DCBOR',
-    globals: {
-        'byte-data': 'byteData',
-        'collections/sorted-map': 'SortedMap',
-    },
+    noExternal: ['byte-data', 'collections/sorted-map'],
 })
